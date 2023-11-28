a69bb2d1747a12f9ef7f9c59f9b4246d10ecd83c
LinHeLurking <LinHe.Lurking@gmail.com>
2023-11-28 11:33:17 +0800

    Optimize startup time a little

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,16 @@
-Set-PSReadLineOption -PredictionSource Plugin
+# Disable history prediction
+# Set-PSReadLineOption -PredictionSource Plugin
 
 $ENV:HTTP_PROXY = "http://127.0.0.1:1089"
 $ENV:HTTPS_PROXY = "http://127.0.0.1:1089"
 
-if (Get-Command gpgconf.exe -ErrorAction SilentlyContinue) {
-    Start-Job -ScriptBlock { gpgconf.exe --launch gpg-agent } | Out-Null
+function Use-Gpg {
+    if (Get-Command gpgconf.exe -ErrorAction SilentlyContinue) {
+        gpgconf.exe --launch gpg-agent
+        # Start-Job -ScriptBlock { gpgconf.exe --launch gpg-agent } | Out-Null
+    }
 }
+
 function Use-VS {
     $VsWhere = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
     $VsInstallDir = & $VsWhere -latest -property installationPath
@@ -22,32 +27,25 @@ function Use-Docker {
     Import-Module DockerCompletion
 }
 
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\LinHe\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\LinHe\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+function Use-Conda {
+    #region conda initialize
+    # !! Contents within this block are managed by 'conda init' !!
+    If (Test-Path "C:\Users\LinHe\miniconda3\Scripts\conda.exe") {
+        (& "C:\Users\LinHe\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
+    }
+    #endregion
 }
-#endregion
 
-# Clear blue bg for `ls` directories
-$PSStyle.FileInfo.Directory = "`e[34m"
+function Get-All {
+    # Clear blue bg for `ls` directories
+    $PSStyle.FileInfo.Directory = "`e[34m"
 
-Use-Git
+    Use-Gpg
+    Use-Git
+    Use-Conda
+    # Use-VS
+}
 
-# Remember to change python env settings
-# {
-#     "background": "#FFDE57",
-#     "foreground": "#111111",
-#     "invert_powerline": true,
-#     "powerline_symbol": "\ue0b2",
-#     "properties": {
-#       "display_mode": "environment",
-#       "fetch_virtual_env": true,
-#       "home_enabled": true
-#     },
-#     "style": "powerline",
-#     "template": " \ue235 {{ if .Error }}{{ .Error }}{{ else }}{{ if .Venv }}{{ .Venv }} {{ else }}{{ .Full }}{{ end }}{{ end }} ",
-#     "type": "python"
-#   },
+Start-Job -ScriptBlock { Get-All } | Out-Null
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
-
